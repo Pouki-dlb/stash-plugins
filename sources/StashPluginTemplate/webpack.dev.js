c8904daad165e5f7a78d5cbc6ec0cb199791b852
Pouki <brochetb@gmail.com>
2026-08-03 14:23:58 +0200

    Delete sources/StashPluginTemplate directory

--- sources/StashPluginTemplate/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-
-// Build de développement : code lisible, source maps pour déboguer dans la
-// console du navigateur. Sortie plus volumineuse, mais on s'en moque ici.
-module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-});
